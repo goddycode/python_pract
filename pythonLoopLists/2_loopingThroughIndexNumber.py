6e677e8217e26815c9293464fbd14cfe1ed09b29
goddycode <odhisgoddy24@gmail.com>
2024-01-17 23:30:58 +0000

    Adding '2_loopingThroughIndexNumber.py '

---
+++ pythonLoopLists/2_loopingThroughIndexNumber.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a python List
+"""
+
+
+def printListForLoop():
+    myList = ["melbbery", "apple", "oranges", "mango"]
+    """Looping through the Index Numbers using range() and len() to create a suitable iterable"""
+    for i in range(len(myList)):
+        print(i)
+
+
+printListForLoop()
