129804aea9ff90f119c22c571d22a508b8658e91
goddycode <odhisgoddy24@gmail.com>
2024-01-17 10:12:18 +0000

    Adding '7_usingGlobalKeyword.py'

---
+++ 7_usingGlobalKeyword.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+If you use the global keyword, the variable belongs to the
+ global scope:
+"""
+
+def myfunc():
+    global x
+    x = "fantastic"
+
+
+myfunc()
+
+
+print("Python is "+x)
