2d632bf463a1bb5d3ebe83c91f4a17d12428d28f
goddycode <odhisgoddy24@gmail.com>
2024-01-17 10:54:13 +0000

    Adding ' 9_pythonList.py'

---
+++ 9_pythonList.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a python List
+"""
+
+
+def printList():
+    global x
+    x = ["apple", "banana", " cherry"]
+
+
+printList()
+
+
+print("Example of a python list is " +x)
