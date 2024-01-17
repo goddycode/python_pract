8c5ec204d303f98b38e0dddba152083c910abd07
goddycode <odhisgoddy24@gmail.com>
2024-01-17 23:31:42 +0000

    Adding '3_whileLoop.py '

---
+++ pythonLoopLists/3_whileLoop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a python List
+"""
+
+
+def printListForLoop():
+    myList = ["melbbery", "apple", "oranges", "mango"]
+    i = 0
+    while i < len(myList):
+        print(i)
+        i = i + 1
+
+
+printListForLoop()
