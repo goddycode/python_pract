ddf69852f3068a690f0ea03063c0796a25a84ec7
goddycode <odhisgoddy24@gmail.com>
2024-01-17 11:18:17 +0000

    Adding '10_pythonDict.py'

---
+++ 10_pythonDict.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a python Dict
+"""
+
+
+def printDict():
+    global x
+    x = {"Name" : "Goddy", "Profession": "Software Engineer", "County": "Kisii"}
+
+
+printDict()
+
+
+print( x)
