a23c668537c9e176b0ad955d631b31e52695545f
goddycode <odhisgoddy24@gmail.com>
2024-01-19 07:54:51 +0000

    '8_functionWithArbitraryArgs.py'

---
+++ pythonLoopLists/8_functionWithArbitraryArgs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is arbitrary arguments, *args function
+"""
+
+
+def arbitrary_ArgumentFunction(*Gtech_Employees):
+    print("The youngest employee is " +Gtech_Employees[2])
+
+
+arbitrary_ArgumentFunction("Michael", "John", "Kenneth")
