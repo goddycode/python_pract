9e9b0ef96264f85cd1f6db22990ed7617b76a1d5
goddycode <odhisgoddy24@gmail.com>
2024-01-19 07:07:38 +0000

    Adding '2_classObject.py'

---
+++ pythonClassesAndObjects/2_classObject.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is class which is a blueprint of creating an object
+"""
+
+
+class MyClass:
+    """This class has a property called x"""
+    x = 5
+
+
+"""This is instance of class MyClass"""
+p1 = MyClass()
+print(p1.x)
