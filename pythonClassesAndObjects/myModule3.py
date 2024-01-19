c4afbcba02290a76a2fd89193b33c03c1371fc79
goddycode <odhisgoddy24@gmail.com>
2024-01-19 07:13:57 +0000

    Adding 'myModule3.py'

---
+++ pythonClassesAndObjects/myModule3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This script illustrates Python module"""
+
+def greeting(name):
+    print(f"Hello, {name}")
+
+
+person1 = {
+    "name": "Goddy",
+    "age": 36,
+    "country": "Kenya"}
