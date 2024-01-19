4dc8f465a840133fe608ac66236f2d231eb34385
goddycode <odhisgoddy24@gmail.com>
2024-01-19 07:10:15 +0000

    Adding ' 7_importModulePart.py'

---
+++ pythonClassesAndObjects/7_importModulePart.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This script illustrate how to import part ofthe module.
+For instance, we are only going to import person1 from 
+mymodule3.py""" 
+
+from myModule3 import person1
+
+print(person1["age"])
