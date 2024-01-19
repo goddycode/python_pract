bbc14f06a7dafaf56282efdf6e4eb687a1e5067a
goddycode <odhisgoddy24@gmail.com>
2024-01-19 07:17:56 +0000

    Adding '13_tryExceptFinally.py  '

---
+++ pythonClassesAndObjects/13_tryExceptFinally.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This script illustrates try_except_finally"""
+
+
+try:
+    print("Hello world")
+except:
+    print("Something else went wrong")
+finally:
+    print("The 'try except' is finished")
