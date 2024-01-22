977c5b474afac8ebbbf9abc5e0ab649c85d8bd81
goddycode <odhisgoddy24@gmail.com>
2024-01-22 16:35:35 +0000

    Adding '2_mysqlConnector.py'

---
+++ python_MySQL/2_mysqlConnector.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Script illustrate mysql - python connector
+"""
+import mysql.connector
+
+
+mydb = mysql.connector.connect(host = "localhost", user = "goddy", password = "password")
+
+print(mydb)
