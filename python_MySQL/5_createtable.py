d3ceb619dd413e540887ebbeecce3d02fb9dcd92
goddycode <odhisgoddy24@gmail.com>
2024-01-22 16:37:18 +0000

    Adding '5_createtable.py'

---
+++ python_MySQL/5_createtable.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Script illustrate mysql - python connector
+"""
+import mysql.connector
+
+
+mydb = mysql.connector.connect(host = "localhost", user = "goddy", password = "password", database ="mydatabase")
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customer (Fname VARCHAR(255), Sname VARCHAR(255), age int)")
